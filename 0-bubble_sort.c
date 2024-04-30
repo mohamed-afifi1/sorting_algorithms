8362cadcc42cc509d291a24449ecab069efe57d1
root <root@DESKTOP-HU468P5.localdomain>
2024-05-01 02:55:51 +0300

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -9,23 +9,22 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-if (size == 0 || array == NULL)
-return;
-int i, j = 0;
+	size_t i, j;
 
+if (array == NULL || size == 0)
+return;
 	for (i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - 1; j++)
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-			int temp = array[j];
+				int temp = array[j];
 
-			    array[j] = array[j + 1];
-			    array[j + 1] = temp;
-			    print_array(array, size);
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				print_array(array, size);
 			}
 		}
 	}
-
 }
