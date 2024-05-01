3f5e7072f74d22730d1e27c7089be0865c49dc3f
root <root@DESKTOP-HU468P5.localdomain>
2024-05-01 04:56:36 +0300

    insertion

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -8,4 +8,42 @@
 */
 void insertion_sort_list(listint_t **list)
 {
+	listint_t *p, *t;
+
+	if (!list)
+		return;
+
+	p = *list;
+
+	while (p)
+	{
+		while (p->next && (p->n > p->next->n))
+		{
+			t = p->next;
+			p->next = t->next;
+			t->prev = p->prev;
+
+			if (p->prev)
+			{
+				p->prev->next = t;
+			}
+			if (t->next)
+			{
+				t->next->prev = p;
+			}
+			p->prev = t;
+			t->next = p;
+
+			if (t->prev)
+			{
+				p = t->prev;
+			}
+			else
+			{
+				*list = t;
+			}
+			print_list(*list);
+		}
+		p = p->next;
+	}
 }
