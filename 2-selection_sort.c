f632b51b9350c5bb366111ce6225f9be43f30728
root <root@DESKTOP-HU468P5.localdomain>
2024-05-01 03:32:06 +0300

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -17,18 +17,18 @@ void selection_sort(int *array, size_t size)
 		min = i;
 		for (j = i + 1; j < size; j++)
 		{
-		if (array[j] < array[min])
-		{
-		min = j;
-		}
+			if (array[j] < array[min])
+			{
+				min = j;
+			}
 
 		}
 		if (i != min)
-	{
-		temp = array[i];
-		array[i] = array[min];
-		array[min] = temp;
-		print_array(array, size);
-	}
+		{
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
 }
 }
