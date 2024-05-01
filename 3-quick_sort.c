248bd45bedd80222cd847232b37d55ee5efc880e
root <root@DESKTOP-HU468P5.localdomain>
2024-05-01 04:12:48 +0300

    quick

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -10,4 +10,46 @@
 */
 void quick_sort(int *array, size_t size)
 {
+	_qsort(array, 0, size - 1);
+}
+
+/**
+* _qsort - quick sort function
+* @a: array of integers
+* @low: the lowest element
+* @high: the highest element
+*/
+void _qsort(int *a, int low, int high)
+{
+	int pivot, pointer, i;
+	int tmp;
+
+	if (low < high)
+	{
+		pivot = high;
+		pointer = low;
+		for (i = low; i < high; i++)
+		{
+			if (a[i] < a[pivot])
+			{
+				if (i != pointer)
+				{
+					tmp = a[i];
+					a[i] = a[pointer];
+					a[pointer] = tmp;
+					print_array(a, high + 1);
+				}
+				pointer++;
+			}
+		}
+		if (pointer != pivot && a[pointer] != a[pivot])
+		{
+			tmp = a[pointer];
+			a[pointer] = a[pivot];
+			a[pivot] = tmp;
+			print_array(a, high + 1);
+		}
+		_qsort(a, low, pointer - 1);
+		_qsort(a, pointer + 1, high);
+	}
 }
